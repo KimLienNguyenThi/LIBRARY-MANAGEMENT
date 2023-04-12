6948f32cb3a1b454aec4ce2fc357b9fe22ef0215
Luan <nguyenthanhluanbntsbd@gmail.com>
2023-04-12 16:03:04 +0700

    luan

--- src/test/Test.java
+++ src/test/Test.java
@@ -9,7 +9,7 @@ public class Test {
 	public static void main(String[] args) {
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-			new LoginView().setVisible(true);;
+			new LoginView().setVisible(true);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
--- src/view/MainView.java
+++ src/view/MainView.java
@@ -24,8 +24,10 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 import java.awt.Component;
+import javax.swing.JTextField;
 
 public class MainView extends JFrame {
+	JFrame frame = new JFrame();
 	
 	private JPanel contentPane;
 	private CardLayout cardLayout;
@@ -34,6 +36,10 @@ public class MainView extends JFrame {
 	private JPanel panelTheThanhVien;
 	private JTable tableMuonSach;
 	private JTable table;
+	private JTextField textFieldTimKiemTTV;
+	private JTable table_1;
+	private JTextField textFieldTimKiemQLNhap;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -72,6 +78,11 @@ public class MainView extends JFrame {
 		Image imglibrary = iconlibrary.getImage();
 		Image newImglibrary = imglibrary.getScaledInstance(58, 58, java.awt.Image.SCALE_SMOOTH);
 		ImageIcon newIconlibrary = new ImageIcon(newImglibrary);
+		
+		ImageIcon iconTimKiem = new ImageIcon(MainView.class.getResource("/images/search.png"));
+		Image imgTimKiem = iconTimKiem.getImage();
+		Image newImgTimKiem = imgTimKiem.getScaledInstance(25, 25, java.awt.Image.SCALE_SMOOTH);
+		ImageIcon newIconTimKiem = new ImageIcon(newImgTimKiem);
 		
 		JButton btnLibrary = new JButton("");
 		btnLibrary.addActionListener(new ActionListener() {
@@ -256,15 +267,105 @@ public class MainView extends JFrame {
 	  	JPanel panelPhieuMuon = new JPanel();
 	  	panelPhieuMuon.setBackground(new Color(255, 255, 255));
 	  	cardPanel.add(panelPhieuMuon, "panelHoaDon");
+	  	panelPhieuMuon.setLayout(null);
 	  	
 	  	JPanel panelQLPhieuMuon = new JPanel();
 	  	panelQLPhieuMuon.setBackground(new Color(255, 255, 255));
 	  	cardPanel.add(panelQLPhieuMuon, "panelQuanLySach");
+	  	panelQLPhieuMuon.setLayout(null);
 	  	
 	  	JPanel panelQuanLySach = new JPanel();
 	  	panelQuanLySach.setBackground(new Color(255, 255, 255));
 	  	cardPanel.add(panelQuanLySach, "panelMuonSach");
 	  	panelQuanLySach.setLayout(null);
+	  	
+	  	JPanel panelQuanLyNhap = new JPanel();
+	  	panelQuanLyNhap.setBackground(new Color(255, 255, 255));
+	  	cardPanel.add(panelQuanLyNhap, "panelThongKe");
+	  	panelQuanLyNhap.setLayout(null);
+	  	
+	  	JPanel panel_1 = new JPanel();
+	  	panel_1.setBounds(10, 123, 829, 415);
+	  	panelQuanLyNhap.add(panel_1);
+	  	panel_1.setLayout(null);
+	  	
+	  	table_1 = new JTable();
+	  	table_1.setModel(new DefaultTableModel(
+	  		new Object[][] {
+	  			{null, null, null, null, null, null},
+	  		},
+	  		new String[] {
+	  			"M\u00E3 l\u00F4", "Ng\u00E0y nh\u1EADp", "Thanh to\u00E1n", "Nh\u00E0 cung c\u1EA5p", "\u0110\u1ECBa ch\u1EC9 cung c\u1EA5p", "S\u1ED1 \u0111i\u1EC7n tho\u1EA1i"
+	  		}
+	  	));
+	  	table_1.getColumnModel().getColumn(0).setPreferredWidth(37);
+	  	table_1.getColumnModel().getColumn(1).setPreferredWidth(63);
+	  	table_1.getColumnModel().getColumn(2).setPreferredWidth(74);
+	  	table_1.getColumnModel().getColumn(3).setPreferredWidth(89);
+	  	table_1.getColumnModel().getColumn(4).setPreferredWidth(101);
+	  	
+	  	JScrollPane scrollPane_2 = new JScrollPane(table_1);
+	  	scrollPane_2.setBounds(0, 0, 829, 415);
+	  	panel_1.add(scrollPane_2);
+	  	
+	  	JButton btnSuaQLNhap = new JButton("Sửa");
+	  	btnSuaQLNhap.setFont(new Font("Tahoma", Font.BOLD, 16));
+	  	btnSuaQLNhap.setBounds(10, 67, 120, 45);
+	  	panelQuanLyNhap.add(btnSuaQLNhap);
+	  	
+	  	JButton btnThemQLNhap = new JButton("Thêm");
+	  	btnThemQLNhap.setFont(new Font("Tahoma", Font.BOLD, 16));
+	  	btnThemQLNhap.setBounds(200, 67, 120, 45);
+	  	panelQuanLyNhap.add(btnThemQLNhap);
+	  	
+	  	JButton btnTimKiemQLNhap = new JButton("");
+	  	btnTimKiemQLNhap.setOpaque(true);
+	  	btnTimKiemQLNhap.setBorderPainted(false);
+	  	btnTimKiemQLNhap.setBackground(new Color(226, 255, 153));
+	  	btnTimKiemQLNhap.setBounds(467, 10, 45, 45);
+	  	panelQuanLyNhap.add(btnTimKiemQLNhap);
+	  	btnTimKiemQLNhap.setIcon(newIconTimKiem);
+	  	
+	  	textFieldTimKiemQLNhap = new JTextField();
+	  	textFieldTimKiemQLNhap.setText("  Search");
+	  	textFieldTimKiemQLNhap.setForeground(Color.LIGHT_GRAY);
+	  	textFieldTimKiemQLNhap.setFont(new Font("Tahoma", Font.PLAIN, 20));
+	  	textFieldTimKiemQLNhap.setColumns(10);
+	  	textFieldTimKiemQLNhap.setBackground(new Color(226, 255, 153));
+	  	textFieldTimKiemQLNhap.setBounds(510, 10, 300, 45);
+	  	panelQuanLyNhap.add(textFieldTimKiemQLNhap);
+	  	
+	  	JButton btnXemChiTietLo = new JButton("Xem chi tiết lô");
+	  	btnXemChiTietLo.setFont(new Font("Tahoma", Font.BOLD, 16));
+	  	btnXemChiTietLo.setBounds(661, 68, 149, 45);
+	  	panelQuanLyNhap.add(btnXemChiTietLo);
+	  	
+	  	JLabel lblNewLabel_1 = new JLabel("New label");
+	  	lblNewLabel_1.setIcon(new ImageIcon(MainView.class.getResource("/images/background.png")));
+	  	lblNewLabel_1.setBounds(0, 0, 849, 548);
+	  	panelQuanLyNhap.add(lblNewLabel_1);
+	  	
+	  	panelTheThanhVien = new JPanel();
+	  	cardPanel.add(panelTheThanhVien, "panelTheThanhVien");
+	  	panelTheThanhVien.setLayout(null);
+	  	
+	  	JPanel panel = new JPanel();
+	  	panel.setBounds(10, 122, 829, 415);
+	  	panelTheThanhVien.add(panel);
+	  	
+	  	table = new JTable();
+	  	table.setModel(new DefaultTableModel(
+	  		new Object[][] {
+	  			{null, null, null, null, null, null, null},
+	  		},
+	  		new String[] {
+	  			"M\u00E3 TV", "H\u1ECD t\u00EAn", "S\u0110T", "\u0110\u1ECBa ch\u1EC9", "Th\u1EDDi gian \u0110K", "H\u1EA1n th\u1EBB", "Ph\u00ED \u0111\u0103ng k\u00FD"
+	  		}
+	  	));
+	  	
+	  	JPanel panel_2 = new JPanel();
+	  	panel_2.setBounds(10, 123, 829, 415);
+	  	panelQuanLySach.add(panel_2);
 	  	
 	  	tableMuonSach = new JTable();
 	  	tableMuonSach.setModel(new DefaultTableModel(
@@ -278,44 +379,91 @@ public class MainView extends JFrame {
 	  	tableMuonSach.getColumnModel().getColumn(0).setPreferredWidth(40);
 	  	tableMuonSach.getColumnModel().getColumn(1).setPreferredWidth(40);
 	  	tableMuonSach.getColumnModel().getColumn(8).setPreferredWidth(51);
+	  	panel_2.setLayout(null);
 	  	
 	  	JScrollPane scrollPane = new JScrollPane(tableMuonSach);
-	  	scrollPane.setBounds(10, 178, 829, 360);
-	  	panelQuanLySach.add(scrollPane);
+	  	scrollPane.setBounds(0, 0, 829, 415);
+	  	panel_2.add(scrollPane);
+	  	
+	  	JButton btnThemQLNhap_1 = new JButton("Thêm");
+	  	btnThemQLNhap_1.setFont(new Font("Tahoma", Font.BOLD, 16));
+	  	btnThemQLNhap_1.setBounds(200, 67, 120, 45);
+	  	panelQuanLySach.add(btnThemQLNhap_1);
+	  	
+	  	JButton btnSuaQLNhap_1 = new JButton("Sửa");
+	  	btnSuaQLNhap_1.setFont(new Font("Tahoma", Font.BOLD, 16));
+	  	btnSuaQLNhap_1.setBounds(10, 67, 120, 45);
+	  	panelQuanLySach.add(btnSuaQLNhap_1);
+	  	
+	  	textField = new JTextField();
+	  	textField.setText("  Search");
+	  	textField.setForeground(Color.LIGHT_GRAY);
+	  	textField.setFont(new Font("Tahoma", Font.PLAIN, 20));
+	  	textField.setColumns(10);
+	  	textField.setBackground(new Color(226, 255, 153));
+	  	textField.setBounds(510, 10, 300, 45);
+	  	panelQuanLySach.add(textField);
+	  	
+	  	JButton btnTimKiemQLNhap_1 = new JButton("");
+	  	btnTimKiemQLNhap_1.setOpaque(true);
+	  	btnTimKiemQLNhap_1.setBorderPainted(false);
+	  	btnTimKiemQLNhap_1.setBackground(new Color(226, 255, 153));
+	  	btnTimKiemQLNhap_1.setBounds(467, 10, 45, 45);
+	  	btnTimKiemQLNhap_1.setIcon(newIconTimKiem);
+	  	panelQuanLySach.add(btnTimKiemQLNhap_1);
 	  	
 	  	JLabel lblNewLabel_2 = new JLabel("New label");
 	  	lblNewLabel_2.setIcon(new ImageIcon(MainView.class.getResource("/images/background.png")));
 	  	lblNewLabel_2.setBounds(0, 0, 849, 548);
 	  	panelQuanLySach.add(lblNewLabel_2);
-	  	
-	  	JPanel panelQuanLyNhap = new JPanel();
-	  	panelQuanLyNhap.setBackground(new Color(255, 255, 255));
-	  	cardPanel.add(panelQuanLyNhap, "panelThongKe");
-	  	panelQuanLyNhap.setLayout(null);
-	  	
-	  	panelTheThanhVien = new JPanel();
-	  	cardPanel.add(panelTheThanhVien, "panelTheThanhVien");
-	  	panelTheThanhVien.setLayout(null);
-	  	
-	  	JPanel panel = new JPanel();
-	  	panel.setBounds(10, 195, 829, 343);
-	  	panelTheThanhVien.add(panel);
-	  	
-	  	table = new JTable();
-	  	table.setModel(new DefaultTableModel(
-	  		new Object[][] {
-	  			{null, null, null, null, null, null, null},
-	  		},
-	  		new String[] {
-	  			"M\u00E3 TV", "H\u1ECD t\u00EAn", "S\u0110T", "\u0110\u1ECBa ch\u1EC9", "Th\u1EDDi gian \u0110K", "H\u1EA1n th\u1EBB", "Ph\u00ED \u0111\u0103ng k\u00FD"
-	  		}
-	  	));
-	  	
-	  	scrollPane.setBounds(10, 178, 829, 360);
 	  	panel.setLayout(null);
+	  	
 	  	JScrollPane scrollPane_1 = new JScrollPane(table);
-	  	scrollPane_1.setBounds(0, 0, 829, 343);
+	  	scrollPane_1.setBounds(0, 0, 829, 415);
 	  	panel.add(scrollPane_1);
+	  	
+	  	JButton btnSuaTheThanhVien = new JButton("Sửa");
+	  	btnSuaTheThanhVien.addActionListener(new ActionListener() {
+	  		public void actionPerformed(ActionEvent e) {
+	  		}
+	  	});
+	  	btnSuaTheThanhVien.setFont(new Font("Tahoma", Font.BOLD, 16));
+	  	btnSuaTheThanhVien.setBounds(10, 67, 120, 45);
+	  	panelTheThanhVien.add(btnSuaTheThanhVien);
+	  	
+	  	JButton btnThemTheThanhVien_1 = new JButton("Thêm");
+	  	btnThemTheThanhVien_1.addActionListener(new ActionListener() {
+	  		public void actionPerformed(ActionEvent e) {
+	  			new DialogThemTTVView(frame).setVisible(true);
+	  		}
+	  	});
+	  	btnThemTheThanhVien_1.setFont(new Font("Tahoma", Font.BOLD, 16));
+	  	btnThemTheThanhVien_1.setBounds(200, 67, 120, 45);
+	  	panelTheThanhVien.add(btnThemTheThanhVien_1);
+	  	
+	  	textFieldTimKiemTTV = new JTextField();
+	  	textFieldTimKiemTTV.setBackground(new Color(0xE2FF99));
+	  	textFieldTimKiemTTV.setForeground(new Color(192, 192, 192));
+	  	textFieldTimKiemTTV.setFont(new Font("Tahoma", Font.PLAIN, 20));
+	  	textFieldTimKiemTTV.setText("  Search");
+	  	textFieldTimKiemTTV.setBounds(510, 10, 300, 45);
+	  	panelTheThanhVien.add(textFieldTimKiemTTV);
+	  	textFieldTimKiemTTV.setColumns(10);
+	  	
+	  	
+	  	
+	  	JButton btnTimKiemTTV = new JButton("");
+	  	btnTimKiemTTV.setBackground(new Color(0xE2FF99));
+	  	btnTimKiemTTV.setOpaque(true);
+	  	btnTimKiemTTV.setBorderPainted(false);
+	  	btnTimKiemTTV.setBounds(467, 10, 45, 45);
+	  	panelTheThanhVien.add(btnTimKiemTTV);
+	  	btnTimKiemTTV.setIcon(newIconTimKiem);
+	  	
+	  	JLabel lblNewLabel_5 = new JLabel("New label");
+	  	lblNewLabel_5.setIcon(new ImageIcon(MainView.class.getResource("/images/background.png")));
+	  	lblNewLabel_5.setBounds(0, 0, 849, 548);
+	  	panelTheThanhVien.add(lblNewLabel_5);
 	  	
 
 	}
