e1ae3ab37a715cb96d21d438a0dae8a6266b3c03
Luan <nguyenthanhluanbntsbd@gmail.com>
2023-04-20 11:38:27 +0700

    fix src

--- src/view/Dialog_ThemSach_QLS.java
+++ src/view/Dialog_ThemSach_QLS.java
@@ -4,7 +4,7 @@ import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class DialogThemQLNhapView extends JDialog {
+public class Dialog_ThemSach_QLS extends JDialog {
 	JFrame frame = new JFrame();
 	
 	private JPanel contentPane;
@@ -13,7 +13,7 @@ public class DialogThemQLNhapView extends JDialog {
 	 * Launch the application.
 	 */
 	
-	public DialogThemQLNhapView(JFrame parent){
+	public Dialog_ThemSach_QLS(JFrame parent){
 		super(parent, "Dialog Default", true);
 		this.init();        
 		this.setLocationRelativeTo(null);
@@ -25,7 +25,7 @@ public class DialogThemQLNhapView extends JDialog {
 	 */
 	
 	public void init() {
-		setBounds(100, 100, 800, 480);
+		setBounds(100, 100, 464, 319);
 		contentPane = new JPanel();
 		
 		setContentPane(contentPane);
